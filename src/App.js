6ff868b0df3c250710007927acd03d295a87714e
Aaditya Joshi <38288671+aadityajo@users.noreply.github.com>
2021-06-01 00:17:10 +0530

    Improved ID

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Todo from "./components/TodoList/Todo";
 function App() {
   const [todo, setTodo] = useState([
     {
-      id: 0,
+      id: "e1",
       title:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     },
     {
-      id: 1,
+      id: "e2",
       title: "World",
     },
     {
-      id: 2,
+      id: "e3",
       title: "Bam Bam",
     },
   ]);
 
   const deleteTodo = (index) => {
-    setTodo((prevState) => prevState.filter((_, i) => i !== index));
+    setTodo((prevState) => prevState.filter((x) => x.id !== index));
   };
 
   return (
